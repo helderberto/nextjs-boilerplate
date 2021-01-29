1e6b5e5c4d990c61c8ae9962f0c0959e4c803fb6
Helder Burato Berto <helder.burato@gmail.com>
2021-01-29 18:17:27 +0000

    refactor: move pages to src

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+
+type Props = {
+  title: string;
+};
+
+export default function Home({ title = 'Next.js Boilerplate' }: Props) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>{title}</h1>
+
+        <p >
+          Get started by editing{' '}
+          <code>pages/index.js</code>
+        </p>
+      </main>
+    </div>
+  )
+}
