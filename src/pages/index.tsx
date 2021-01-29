48423f4022fc6a1003ae9bf23ce7187e0463ebcc
Helder Burato Berto <helder.burato@gmail.com>
2021-01-29 20:06:43 +0000

    fix: index.tsx format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 
 type Props = {
-  title: string;
-};
+  title: string
+}
 
 export default function Home({ title = 'Next.js Boilerplate' }: Props) {
   return (
@@ -15,9 +15,8 @@ export default function Home({ title = 'Next.js Boilerplate' }: Props) {
       <main>
         <h1>{title}</h1>
 
-        <p >
-          Get started by editing{' '}
-          <code>pages/index.js</code>
+        <p>
+          Get started by editing <code>pages/index.js</code>
         </p>
       </main>
     </div>
