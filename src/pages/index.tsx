e7215a71f66c18f9d6ab17c2d7b7e74a9b204837
Helder Burato Berto <helder.burato@gmail.com>
2021-02-13 18:55:31 +0000

    refactor(home): cleanup html and just load Main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,5 @@
-import Head from 'next/head'
+import Main from 'components/Main'
 
-type Props = {
-  title: string
-}
-
-export default function Home({ title = 'Next.js Boilerplate' }: Props) {
-  return (
-    <div>
-      <Head>
-        <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1>{title}</h1>
-
-        <p>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-    </div>
-  )
+export default function Home() {
+  return <Main />
 }
