11c583633b77b8c86563e7c13e44c13bc4824efc
Helder Burato Berto <helder.burato@gmail.com>
2021-02-21 13:20:52 +0000

    refactor(main): files export component and test

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,2 +1,20 @@
-/* istanbul ignore file */
-export { default } from './Main'
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Image of an atom and written React Avançado at the side."
+    />
+    <S.Title>Next.js Boilerplate</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS and Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="A developer in front of a screen with code."
+    />
+  </S.Wrapper>
+)
+
+export default Main
