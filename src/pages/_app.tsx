81b470fadd9bfa08ea719651e0bcce0c76be5dc3
Helder Burato Berto <helder.burato@gmail.com>
2021-03-04 18:46:17 +0000

    feat(pwa): configure next-pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ function App({ Component, pageProps }: AppProps) {
         <title>NextJS Boilerplate</title>
         <link rel="shortcut icon" href="/img/favicon.ico" />
         <link rel="apple-touch-icon" href="/img/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
