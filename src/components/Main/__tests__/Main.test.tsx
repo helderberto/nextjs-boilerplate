9531d090ce2598e5a004ea0c1ee36bd508efe6eb
Helder Burato Berto <helder.burato@gmail.com>
2021-05-02 17:36:08 +0100

    test: move tests Main to __tests__

--- src/components/Main/__tests__/Main.test.tsx
+++ src/components/Main/__tests__/Main.test.tsx
@@ -1,13 +1,13 @@
 import { render, screen } from '@testing-library/react'
 
-import Main from '.'
+import Main from '..'
 
 describe('<Main />', () => {
   it('should render the heading', () => {
     render(<Main />)
 
     expect(
-      screen.getByRole('heading', { name: /next.js boilerplate/i })
+      screen.getByRole('heading', { name: /next.js boilerplate/i }),
     ).toBeInTheDocument()
   })
 
