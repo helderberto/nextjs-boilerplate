ad07af1a97f25051d3555755b02a2816609ad88f
Helder Burato Berto <helder.burato@gmail.com>
2021-05-02 17:44:07 +0100

    chore: enable webpack5 next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
@@ -6,5 +5,8 @@ module.exports = withPWA({
   pwa: {
     dest: 'public',
     disable: !isProd,
+  },
+  future: {
+    webpack5: true,
   },
 })
